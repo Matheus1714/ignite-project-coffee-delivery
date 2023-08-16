72d9f7ccf84a800c79365f8831f74c13ded0ef5d
matheus1714 <matheussilvamartins1714@gmail.com>
2023-08-15 23:30:05 -0300

    config styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { GlobalStyle } from './styles/global'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+
 export function App() {
-  return <h1>Hello World</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hello</h1>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
